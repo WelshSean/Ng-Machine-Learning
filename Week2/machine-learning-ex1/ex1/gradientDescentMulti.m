fec2fe2dcd2d915c230c62f1ac14968a3ec5208c
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-12-11 22:33:48 +0000

    Gradient descent multi now working

--- Week2/machine-learning-ex1/ex1/gradientDescentMulti.m
+++ Week2/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -6,7 +6,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 n = size(X, 2); % number of features
-theta_new = zeros(1,length(theta));
+theta_new = zeros(length(theta), 1);
 
 J_history = zeros(num_iters, 1);
 
@@ -14,13 +14,7 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
     
-    % Evaluate h
-    h = zeros(1,length(y));
-    for i = 1:m
-        for j = 1:n
-            h(i) = h(i) + X(i,j)*theta(j);
-        end
-    end
+
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
@@ -29,16 +23,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %calculate the sum of h(x) - y
+    
+    htheta = X * theta
 
     for jj = 1:n    % theta Index loop
-        sum=0;
-        for i = 1:m         % sample loop            
-            sum = (h(i) - y(i)) * X(i,jj);
-        end
-        theta(jj) = theta(jj) - sum * alpha/m;
+            theta_new(jj) = theta(jj) - alpha/m * sum((htheta - y) .* X(:,jj));
     end
-
-
+    
+    theta=theta_new
     % ============================================================
 
     % Save the cost J in every iteration    
